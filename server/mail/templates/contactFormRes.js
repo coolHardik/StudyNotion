0310622b8cdc0a95c11e23aa881447e6c73269e3
coolHardik <137192287+coolHardik@users.noreply.github.com>
2024-09-15 12:18:46 +0530

    Update contactFormRes.js

--- server/mail/templates/contactFormRes.js
+++ server/mail/templates/contactFormRes.js
@@ -74,8 +74,8 @@ exports.contactUsEmail = (
     
     <body>
         <div class="container">
-           <img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo">
+            <a href="https://study-notion-three-mauve.vercel.app/"><img class="logo"
+                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
             <div class="message">Contact Form Confirmation</div>
             <div class="body">
                 <p>Dear ${firstname} ${lastname},</p>
