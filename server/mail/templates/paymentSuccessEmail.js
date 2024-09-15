729017caca14d61800641cae3196df6f4f2c5b9c
coolHardik <137192287+coolHardik@users.noreply.github.com>
2024-09-15 12:26:44 +0530

    Update paymentSuccessEmail.js

--- server/mail/templates/paymentSuccessEmail.js
+++ server/mail/templates/paymentSuccessEmail.js
@@ -67,8 +67,8 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
       
       <body>
           <div class="container">
-              <img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
-                      alt="StudyNotion Logo">
+            <a href="https://study-notion-three-mauve.vercel.app/"><img class="logo"
+                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
               <div class="message">Course Payment Confirmation</div>
               <div class="body">
                   <p>Dear ${name},</p>
